4eb74ecedd3e19efca32b9e6627dc6fc015e7260
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2023-02-17 11:44:21 -0600

    Update Invoke-RaderSec.psm1

--- Invoke-RaderSec.psm1
+++ Invoke-RaderSec.psm1
@@ -891,28 +891,17 @@ Function Invoke-RaderSec {
     #
     #}
     
-    Function UpdateRaderSec {
-    $FolderPath = "$($env:USERPROFILE)\OneDrive - Rader Solutions\Documents"
+    Function UpdateRaderSec{
+    $FilePath = "$env:ProgramFiles\WindowsPowerShell\Modules"
     $Url = "https://github.com/xBurningGiraffe/RaderSecOps/archive/refs/heads/main.zip"
-    $DownloadPath = "$folderPath\RaderSecOps.zip"
-    Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
-    $FileCheck = Test-Path $FolderPath\WindowsPowerShell
-    $ModuleCheck = Test-Path $FileCheck\Modules
-    
-    if ($FileCheck) {
-      New-Item -ItemType Directory -Path $FolderPath\WindowsPowerShell 
-    }
-    if ($ModuleCheck) {
-      New-Item -ItemType Directory -Path $FolderPath\WindowsPowerShell\Modules
-    }
-    
-    Expand-Archive -Path $DownloadPath -DestinationPath $FolderPath\WindowsPowerShell\Modules
-    Move-Item '.\OneDrive - Rader Solutions\Documents\WindowsPowerShell\Modules\RaderSecOps-main\' '.\OneDrive - Rader Solutions\Documents\WindowsPowerShell\Modules\RaderSecOps'
-    Import-Module $FolderPath\WindowsPowerShell\Modules\RaderSecOps\Invoke-RaderSec.psm1
+    Invoke-WebRequest -Uri $Url -OutFile main.zip
+    Expand-Archive .\main.zip -DestinationFolder $FilePath -Force
+    Move-Item $FilePath\RaderSecOps-main\ $FilePath\RaderSecOps
+    Import-Module $Filepath\WindowsPowerShell\Modules\RaderSecOps\Invoke-RaderSec.psm1
     }
     if ($Update) {
         UpdateRaderSec
-        exit
+        break
     }
     
     
