a0a299ebaf4a35d48e6566ad5921384e3c9adc76
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2023-02-17 12:22:28 -0600

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,17 +1,13 @@
-$FolderPath = "$($env:USERPROFILE)\OneDrive - Rader Solutions\Documents"
+$FolderPath = "$($env:ProgramFiles)\WindowsPowerShell\Modules"
 $Url = "https://github.com/xBurningGiraffe/RaderSecOps/archive/refs/heads/main.zip"
 $DownloadPath = "$folderPath\RaderSecOps.zip"
 Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
-$FileCheck = Test-Path $FolderPath\WindowsPowerShell
-$ModuleCheck = Test-Path $FileCheck\Modules
 
-if ($FileCheck) {
-  New-Item -ItemType Directory -Path $FolderPath\WindowsPowerShell -Force
+Expand-Archive $FolderPath\RaderSecOps.zip -DestinationPath $FolderPath -Force
+Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps
+if (Test-Path $Profile) -eq $false)) {
+    New-Item -Path $Profile -ItemType File -Force
+    echo 'Import-Module -Name RaderSecOps' >> $Profile
+} elseif (Test-Path $Profile) -eq $true)) {
+    echo 'Import-Module -Name RaderSecOps' >> $Profile
 }
-if ($ModuleCheck) {
-  New-Item -ItemType Directory -Path $FolderPath\WindowsPowerShell\Modules -Force
-}
-
-Expand-Archive -Path $DownloadPath -DestinationPath $FolderPath\WindowsPowerShell\Modules -Force
-Move-Item '.\OneDrive - Rader Solutions\Documents\WindowsPowerShell\Modules\RaderSecOps-main\' '.\OneDrive - Rader Solutions\Documents\WindowsPowerShell\Modules\RaderSecOps'
-Import-Module $FolderPath\WindowsPowerShell\Modules\RaderSecOps\Invoke-RaderSec.psm1
