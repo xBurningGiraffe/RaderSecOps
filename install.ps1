81959e6ac42ed94e2baf9c0fdcd8b9f3907317f8
xBurningGiraffe <xburninggiraffe@gmail.com>
2023-02-17 16:37:30 -0600

    Update

--- install.ps1
+++ install.ps1
@@ -6,5 +6,5 @@ Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
 Expand-Archive $FolderPath\RaderSecOps.zip -DestinationPath $FolderPath -Force
 Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps
 Write-Output 'Import-Module -Name RaderSecOps' > $Profile
-Write-Output 'Import-Module -Name '($env:ProgramFiles)\WindowsPowerShell\Module\RaderSecOps\Start-IntuneManagement.psm1'' >> $Profile
+Write-Output 'Import-Module -Name "$env:ProgramFiles\WindowsPowerShell\Modules\RaderSecOps\Start-IntuneManagement.psm1"' >> $Profile
 Import-Module -Name RaderSecOps
