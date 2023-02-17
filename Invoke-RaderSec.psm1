161471e641a4e4c0529d3d99c75a11c8f8cb3386
xBurningGiraffe <xburninggiraffe@gmail.com>
2023-02-17 16:42:13 -0600

    Update Invoke-RaderSec.psm1

--- Invoke-RaderSec.psm1
+++ Invoke-RaderSec.psm1
@@ -899,9 +899,9 @@ Function UpdateRaderSec{
     Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
 
     Expand-Archive $FolderPath\RaderSecOps.zip -DestinationPath $FolderPath -Force
-    Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps
+    Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps -Force
     Write-Output 'Import-Module -Name RaderSecOps' > $Profile
-    Write-Output 'Import-Module -Name '($env:ProgramFiles)\WindowsPowerShell\Module\RaderSecOps\Start-IntuneManagement.psm1'' >> $Profile
+    Write-Output 'Import-Module -Name "$env:ProgramFiles\WindowsPowerShell\Module\RaderSecOps\Start-IntuneManagement.psm1"' >> $Profile
     Import-Module -Name RaderSecOps
 }
     if ($Update) {
