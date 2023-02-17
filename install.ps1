e16e691989ca336e1777660e6b30e9d3a78d0f10
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2023-02-17 12:24:19 -0600

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,10 +5,5 @@ Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
 
 Expand-Archive $FolderPath\RaderSecOps.zip -DestinationPath $FolderPath -Force
 Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps
-if (Test-Path $Profile) -eq $false)) {
-    New-Item -Path $Profile -ItemType File -Force
-    echo 'Import-Module -Name RaderSecOps' >> $Profile
-} elseif (Test-Path $Profile) -eq $true)) {
-    echo 'Import-Module -Name RaderSecOps' >> $Profile
-}
+echo 'Import-Module -Name RaderSecOps' >> $Profile
 Import-Module -Name RaderSecOps
