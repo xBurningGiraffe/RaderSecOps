64e69a15f2ab44ae031903a113801cf74235c934
xBurningGiraffe <xburninggiraffe@gmail.com>
2023-03-06 14:48:09 -0600

    Update fix
    Altered -Update to be more specific

--- Invoke-RaderSec.psm1
+++ Invoke-RaderSec.psm1
@@ -775,17 +775,18 @@ Function PhishButton {
 
 
 Function Updates{
-    $FolderPath = "$($env:ProgramFiles)\WindowsPowerShell\Modules"
+    $FolderPath = "$env:ProgramFiles\WindowsPowerShell\Modules"
     $Url = "https://github.com/xBurningGiraffe/RaderSecOps/archive/refs/heads/main.zip"
-    $DownloadPath = "$folderPath\RaderSecOps.zip"
+    $DownloadPath = "$FolderPath\RaderSecOps.zip"
     Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
-
     Expand-Archive $FolderPath\RaderSecOps.zip -DestinationPath $FolderPath -Force
-    Remove-Item $FolderPath\RaderSecOps -Recurse -Force
-    Remove-Item $FolderPath\RaderSecOps-main -Recurse -Force
-    Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps -Force
+    Remove-Item $FolderPath\RaderSecOps -Recurse -Force -ErrorAction SilentlyContinue
+   # Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps -Force
+   $CheckProfile = (Get-Content $Profile)
+   if ($null -eq $CheckProfile) {
     Write-Output 'Import-Module -Name RaderSecOps' > $Profile
     Write-Output 'Import-Module -Name "$env:ProgramFiles\WindowsPowerShell\Modules\RaderSecOps\Start-IntuneManagement.psm1"' >> $Profile
+   }
     Import-Module -Name RaderSecOps
 }
 if ($Updates) 
