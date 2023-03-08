8c8bd0a88ad1a1b83fbc23ddfa22f93f3143d2a7
xBurningGiraffe <xburninggiraffe@gmail.com>
2023-03-08 13:42:06 -0600

    last edit
    Fixed stupid update again

--- Invoke-RaderSec.psm1
+++ Invoke-RaderSec.psm1
@@ -784,7 +784,7 @@ Function Updates{
     Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
     Expand-Archive $FolderPath\RaderSecOps.zip -DestinationPath $FolderPath -Force
     Remove-Item $FolderPath\RaderSecOps -Recurse -Force -ErrorAction SilentlyContinue
-   # Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps -Force
+    Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps -Force
    $ImportRadersec = 'Import-Module -Name RaderSecOps'
    $ImportIntune = 'Import-Module -Name "$env:ProgramFiles\WindowsPowerShell\Modules\RaderSecOps\Start-IntuneManagement.psm1"'
    $CheckProfile = (Get-Content $Profile)
