9b5191b0f7736a97fa037a7de4444ed6bcbb056e
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2023-03-08 15:26:44 -0600

    Update radersecinstall.ps1

--- radersecinstall.ps1
+++ radersecinstall.ps1
@@ -1,15 +1,30 @@
-    $FolderPath = "$env:ProgramFiles\WindowsPowerShell\Modules"
-    $Url = "https://github.com/xBurningGiraffe/RaderSecOps/archive/refs/heads/main.zip"
-    $DownloadPath = "$FolderPath\RaderSecOps.zip"
-    Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
-    Expand-Archive $FolderPath\RaderSecOps.zip -DestinationPath $FolderPath -Force
-    Remove-Item $FolderPath\RaderSecOps -Recurse -Force -ErrorAction SilentlyContinue
-    Move-Item $FolderPath\RaderSecOps-main $FolderPath\RaderSecOps -Force
+  # Pull RaderSecOps from GitHub
+    Invoke-WebRequest -Uri https://github.com/xBurningGiraffe/RaderSecOps/archive/refs/heads/main.zip -OutFile main.zip
+    Expand-Archive main.zip -DestinationPath $env:ProgramFiles\WindowsPowerShell\Modules\main -Force
+    if (Get-ChildItem -Path 'C:\Program Files\windowspowershell\Modules\RaderSecOps') {
+    try {
+     Remove-Item 'C:\Program Files\windowspowershell\Modules\RaderSecOps' -Recurse -ErrorAction SilentlyContinue
+     }
+     catch {
+     Write-Error "Error: $($_.Exception.Message)"
+     }
+    Move-Item 'C:\Program Files\windowspowershell\Modules\main\RaderSecOps-main' 'C:\Program Files\windowspowershell\Modules\RaderSecOps' -Force
+    Remove-Item 'C:\Program Files\windowspowershell\Modules\main' -Recurse
+    # Import new modules
+    Import-Module -Name RaderSecOps
+    Import-Module -Name 'C:\Program Files\WindowsPowerShell\Modules\RaderSecOps\IntuneManagement.psd1'
+    # Add module imports to $Profile
    $ImportRadersec = 'Import-Module -Name RaderSecOps'
-   $ImportIntune = 'Import-Module -Name "$env:ProgramFiles\WindowsPowerShell\Modules\RaderSecOps\Start-IntuneManagement.psm1"'
+   $ImportIntune = 'Import-Module -Name "C:\Program Files\WindowsPowerShell\Modules\RaderSecOps\IntuneManagement.psd1"'
    $CheckProfile = (Get-Content $Profile)
    if ($CheckProfile -notcontains $ImportRadersec -or $ImportIntune) {
     Write-Output 'Import-Module -Name RaderSecOps' > $Profile
     Write-Output 'Import-Module -Name "$env:ProgramFiles\WindowsPowerShell\Modules\RaderSecOps\Start-IntuneManagement.psm1"' >> $Profile
-}
-Remove-Item $DownloadPath
+    }
+    Remove-Item main.zip
+    try {
+    Remove-Item radersecinstall.ps1
+    }
+    catch {
+    Write-Error "Error "Error: $($_.Exception.Message)"
+    }
