af502f207e7052454a596a642b0411eab74376da
xBurningGiraffe <xburninggiraffe@gmail.com>
2023-04-12 14:26:51 -0500

    More updates again :]

--- Invoke-RaderSec.psm1
+++ Invoke-RaderSec.psm1
@@ -815,16 +815,35 @@ Function Rader_IPHunter {
 Function UpdateRaderSec{
     $FolderPath = "$($env:ProgramFiles)\WindowsPowerShell\Modules"
     $Url = "https://github.com/xBurningGiraffe/RaderSecOps/archive/refs/heads/main.zip"
-    $DownloadPath = "$folderPath\RaderSecOps.zip"
-    Remove-Item $FolderPath\RaderSecOps -Recurse -Force
+    $DownloadPath = "$FolderPath\RaderSecOps.zip"
+    $ExtractPath = "$FolderPath\RaderSecOps"
+
+    # Check if the RaderSecOps folder exists and remove it if it does
+    if (Test-Path "$FolderPath\RaderSecOps") {
+        Remove-Item "$FolderPath\RaderSecOps" -Recurse -Force
+    }
+    
+    # Download the RaderSecOps module from the URL
     Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
 
-    Expand-Archive $FolderPath\RaderSecOps.zip -DestinationPath $FolderPath -Force
-    Move-Item $FolderPath\RaderSecOps\RaderSecOps-main\* $FolderPath\RaderSecOps -Force
-    Remove-Item $FolderPath\RaderSecOps\RaderSecOps-main -Confirm:$false
-    Write-Output 'Import-Module -Name RaderSecOps' > $Profile
-    Write-Output 'Import-Module -Name "$env:ProgramFiles\WindowsPowerShell\Modules\RaderSecOps\Start-IntuneManagement.psm1"' >> $Profile
-    Import-Module -Name RaderSecOps
+    # Extract the contents of the .zip file to a temporary folder
+    $TempPath = "$FolderPath\RaderSecOps-main"
+    Expand-Archive -Path $DownloadPath -DestinationPath $TempPath
+
+# If the extracted folder is named "RaderSecOps-main", move its contents to $ExtractPath
+    if (Test-Path "$TempPath\RaderSecOps-main") {
+        Move-Item "$TempPath\RaderSecOps-main\*" -Destination $ExtractPath
+    }
+
+# Remove the temporary folder
+    Remove-Item $TempPath -Recurse -Force
+
+    $ProfilePath = "$($env:USERPROFILE)\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+      if (!(Get-Content $ProfilePath | Select-String -SimpleMatch 'Import-Module -Name RaderSecOps') -or !(Get-Content $ProfilePath | Select-String -SimpleMatch 'Import-Module -Name "$env:ProgramFiles\WindowsPowerShell\Modules\RaderSecOps\Start-IntuneManagement.psm1"')) {
+    Write-Output 'Import-Module -Name RaderSecOps' >> $ProfilePath
+    Write-Output 'Import-Module -Name "$env:ProgramFiles\WindowsPowerShell\Modules\RaderSecOps\Start-IntuneManagement.psm1"' >> $ProfilePath
+    }
+    Write-Host "RaderSecOps has been updated. Restart RaderSecOps in a new tab or Powershell window." -ForegroundColor DarkGreen
 }
 
 
@@ -841,12 +860,7 @@ Function UpdateRaderSec{
     }
     }
 
-    if (Get-PSSession | Where-Object { $_.ConfigurationName -eq 'Microsoft.Exchange' }) {
-        Disconnect-ExchangeOnline -Confirm:$false -InformationAction Ignore -ErrorAction SilentlyContinue
-    } else {
-        Write-Output 'You are not currently connected to Exchange Online.'
-    }
-    
+
     # Function for disconnecting and breaking
     Function Goodbye {
         Write-Host "----------------------------------------------------------------------------------------------------------------------------------" -ForegroundColor DarkGreen
